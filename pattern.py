51b7d7d5b1bd438c98c354f9a546c97bbe2fefa3
atheis4 <atheis@uw.edu>
2018-02-17 16:40:38 -0800

    implemented the movement map... still need to hammer home the last few pieces in order to create the pattern for a given duration (number of pentagons)

--- pattern.py
+++ pattern.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class Origin(object):
     """
@@ -5,27 +7,21 @@ class Origin(object):
     coordinate on the wire_frame of the peice.
 
     Arguments:
-        row_index (int):
-        col_index (int):
-        row_height (int):
-        col_height (int):
-        tile_one (int):
-        tile_two (int):
+        row_index (int): row position on WireFrame.frame
+        col_index (int): column position on WireFrame.frame
+        dimension (int): which
+        init_tile (int):
     """
 
     def __init__(self,
                  row_index=0,
                  col_index=0,
-                 row_height=0,
-                 col_height=1,
-                 tile_one=0,
-                 tile_two=1):
+                 dimension=0
+                 init_tile=0):
         self._row_index = row_index
         self._col_index = col_index
-        self._row_height = row_height
-        self._col_height = col_height
-        self._tile_one = tile_one
-        self._tile_two = tile_two
+        self._dimension = dimension
+        self._init_tile = init_tile
 
 
 class SpiralArm(object):
@@ -58,23 +54,87 @@ class SpiralArm(object):
 
 
 class SquareSpiralArm(Spiral):
+    """
+    The origin is point 0. A square spiral arm is the most basic meta pattern
+    applied to the WireFrame.
+
+    A movement map tracks which type of incrementing and decrementing we need
+    to apply to the current piece's coordinates in order to advance the
+    pattern.
+    """
+
     def __init__(self, rotation='cw', space='pos', origin=None):
         super().__init__(rotation, space, origin)
+        self.current_movement = self.movement_map[0]
+        self.pivot_total = 0
 
-    def run(self):
-        pass
+    @classmethod
+    def negative_row_positive_col(cls):
+        """
+        Decrement the row by one or zero.
+        Increment the column by one or zero.
+        """
+        delta = np.array([[0, 1], [1, 0]], [[0, 0], [0, 0]])
 
-    def return_next(self, current, pivot=None):
-        pass
+    @classmethod
+    def positive_row_positive_col(cls):
+        """
+        Increment the row by one or zero.
+        Increment the column by one or zero.
+        """
+        delta = np.array([[0, 0], [1, 0]], [[1, 0], [0, 0]])
 
+    @classmethod
+    def negative_row_negative_col(cls):
+        """
+        Decrement the row by one or zero.
+        Decrement the column by one or zero.
+        """
+        delta = np.array([[0, 1], [0, 0]], [[0, 0], [0, 1]])
 
-class Peice(object):
-    pass
+    @classmethod
+    def positive_row_negative_col(cls):
+        """
+        Increment the row by one or zero.
+        Decrement the column by one or zero.
+        """
+        delta = np.array([[0, 0], [0, 0]], [[0, 1], [1, 0]])
 
+    def movement_map(self):
+        """
+        Should be a Queue that rotates by adding the current movement to the
+        end of the Queue while progressing to the next step...
 
-class PeiceOneThree(Peice):
-    pass
+        This way, the only difference between clockwise and counterclockwise is
+        to advance to the next element or the last element.
+        """
+        if self.rotation == 'cw':
+            return [self.negative_row_positive_col,
+                    self.positive_row_positive_col,
+                    self.positive_row_negative_col,
+                    self.negative_row_negative_col]
+        else:
+            return [self.negative_row_negative_col,
+                    self.positive_row_negative_col,
+                    self.positive_row_positive_col,
+                    self.negative_row_positive_col]
 
+    def pivot(self):
+        """
+        Counts the pivots and tells the pattern when to advance to the next
+        set of movements in the movement map.
+        """
+        self.pivot += 1
+        self.current_movement = self.movement_map[self.current_movement + 1]
 
-class PeiceOneFour(Peice):
-    pass
+    def run(self, duration=400):
+        """
+        The core algorithm.
+
+        Combines the incrementing with tracking pivoting and detailing which
+        type of motion is necessary to continue the pattern.
+        """
+        for i in range(1, duration):
+            is_pivot = np.sqrt(i) == int(np.sqrt(i))
+            if is_pivot:
+                self.pivot()
